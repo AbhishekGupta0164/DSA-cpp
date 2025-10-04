f48b88fd5e46ed01fce95677e92cfaa6ffdbe2ba
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2025-10-04 19:02:23 +0530

    Re-added recursion as normal folder with cpp files

---
+++ recursion/recursion/recu01.cpp
@@ -0,0 +1,20 @@
+// to print a sum of 10 natural no
+#include <iostream>
+using namespace std;
+int sum(int y)
+{
+    if (y == 0)
+        return 0;
+    else
+        return (y + sum(y - 1));
+}
+int main()
+{
+    int y;
+    cout << "enter no";
+    cin >> y;
+    cout << "the sum of first " << y << "integer = " << sum(y) << endl;
+    return 0;
+
+}
+// 
