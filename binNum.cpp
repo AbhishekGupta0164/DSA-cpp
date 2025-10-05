72ee179dbf59f0de1ac92d95c2b0af3abca141b5
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2025-10-05 22:13:00 +0530

    BinaryNumConversion

---
+++ binNum.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+using namespace std;
+
+//  *Binary number system conversion
+
+ //  01   decimal to binary
+
+int binToDecimal(int binNum ){
+    int ans =0 ; int pow =1;
+
+    while(binNum >0){
+        int rem = binNum % 10;
+        ans += rem* pow;
+        binNum /= 10;
+        pow *=2;
+    }
+    return ans ;  //decimal form
+      
+}
+int main(){
+    cout<<binToDecimal(1010)<< endl; //10
+    return 0;
+
+}
+
+
+//******************************* 
+
+//**function  call codes 
+
+
+//         //  01  sum of digit 
+//   int sumofdigits(int num) {
+//     int digsum =0;
+//   while(num>0){
+//     int lastdig = num% 10;
+//     num/=10;
+//     digsum += lastdig;
+
+//   }
+//   return digsum;
+// }
+// int main(){
+//     cout<<"sum = "<<sumofdigits(2839)<<endl;
+//     return 0;
+// }
+
+
+
+//  //   02    sum of two no and its value  to print
+// int sum(int a ,int b){
+//     a=a+10;
+//     b=b+10;
+//     return a+b;
+    
+
+// }
+// int main(){
+//     int a=5,b=4;
+//     cout<<sum(a,b) <<endl;
+//     cout<<a<<b<<endl;
+//     return 0;
+// }   
+
+
+  // // ******************************************
+
+//  //  *Bitwise operator
+
+//  //01  AND &
+// int main(){
+//  int a= 4,b=8;
+//     cout<< (a&b)<<endl;
+//     return 0;
+// }
+
+// //02  OR |
+// int main(){
+//     int a=4,b=8;
+//     cout<<(a|b)<<endl;
+//     return 0;
+// }
+
+// // 03 XOR
+// int main(){
+//     int a=4,b=8;
+//     cout<<(a^b)<<endl;
+//     return 0;
+// }
