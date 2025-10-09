10bfbb65219c5d9bd59849df178b430f91bb237f
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2025-10-09 21:51:58 +0530

    Vector - to print the element that is unique in array

---
+++ Array02/arr02.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+# include<vector>
+using namespace std;
+// leetcode problem 136
+// to print the element that is unique in array where every other element appears twice
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+          int ans =0;
+          for(int val: nums){
+            ans =ans^val;
+          }
+          return ans;  
+    }
+};
+
+int main() {
+    vector<int> nums = {4, 1, 2, 1, 2};
+    Solution sol;
+    cout << "The unique number is: " << sol.singleNumber(nums) << endl;
+    return 0;
+}
