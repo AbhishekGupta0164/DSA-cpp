9c9eb393ee8138c2946b5c4c7e0c43ae63662694
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2025-10-11 21:33:10 +0530

    to print Max Subarrays and their sum

--- Array02_vector/MaxSubArrSum.cpp
+++ Array02_vector/MaxSubArrSum.cpp
@@ -1,5 +1,39 @@
 #include<iostream>
-# include<vector>
+#include<climits>
+using namespace std;
+
+ // to print Max Subarrays and their sum 
+
 int main(){
+    int n = 5;
+    int arr[n] ={1,2,3,4,5};
+    int maxSum = INT_MIN;
+
+   //  //  to print SubArrays
     
+    // for(int st=0;st<=n;st++){
+    //     for(int end =st; end<n;end++){
+    //        for(int i=st; i<=end;i++){
+    //            cout<< arr[i];
+    //        }
+    //        cout<<" ";
+    //     }
+    //     cout<< endl;
+    // }
+
+    // **************************
+     
+    // // to print Max SubArray Sum
+    //// current sum =  0 at starting point
+ 
+    for(int st=0;st<=n;st++){
+       int currSum =0;
+        for(int end =st; end<n;end++){
+           currSum +=arr[end];
+            maxSum = max(currSum ,maxSum);
+        }
+    }
+     cout<<"max subarray sum = "<<maxSum<<endl;
+    
+    return 0;
 }
