57254d48af1757aa904b9b0db8a232476cb94af3
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2025-10-12 22:44:06 +0530

    Majority element in an array

---
+++ Array02_vector/majorityelement.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+ // Majority element in an array
+int main(){
+    int arr[] = {2,6,4,5,2,1};
+    int len = sizeof(arr)/sizeof(arr[0]);
+    int count = 0;
+    int candidate = arr[0];
+
+    for (int i = 0; i < len; i++) {
+        if (count == 0) {
+            cout << "here " << i << endl;
+            candidate = arr[i];
+            count = 1;
+        } else {
+            if (arr[i] == candidate)
+                count++;
+            else
+                count--;
+        }
+    }
+
+    cout << "Majority Element: " << candidate << endl;
+    return 0;
+}
